2b612b48a3312c1b67dceaaea7cd354806b7b800
天成 石 <stc1995@sjtu.edu.cn>
2020-09-10 16:14:52 +0800

    添加项目文件。

---
+++ commodity.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "commodity.h"
+//≥ı ºªØID
+int commodity::kinds  = 0;
+void commodity::UpdateStock(int n)
+{
+		stock+=n;
+}
+int commodity::getStock()
+{
+		return stock;
+}
+int commodity::getID()
+{
+		return ID;
+}
+float commodity::getBuyValue()
+{
+		return buyValue;
+}
+float commodity::getsellValue()
+{
+		return sellValue;
+}
+char* commodity::getName()
+{
+		return name;
+}
